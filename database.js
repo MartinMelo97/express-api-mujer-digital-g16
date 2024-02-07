f8cf607bb791dcf4698e0b2d2baba8ed0a8d2478
Martin Melo <martin.melo.dev.97@gmail.com>
2024-02-06 19:01:33 -0600

    Starting movies CRUD API

---
+++ database.js
@@ -0,0 +1,9 @@
+const movies = [
+    { id: 1, name: 'Radical', duration: 150, sinopsis: 'Está chida', cast: ['Eugenio Derbez'] },
+    { id: 2, name: 'Sherk', duration: 100, sinopsis: 'Me hizo llorar', cast: ['Eugenio Derbez', 'Antonio Balderas'] },
+    { id: 3, name: 'Toy Story', duration: 120, sinopsis: 'Yo soy tu amigo fiel', cast: ['Woody', 'Buzz'] },
+];
+
+module.exports = {
+    movies,
+}
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { movies } = require('./database');
 
 dotenv.config();
 
 const app = express();
 
+// -- Rutas de películas ------------------------------------------------
+app.get('/movies', function(req, res) {
+    return res.status(200).json(movies);
+});
+
+app.get('/movies/:id', function(req, res) {
+    const { id } = req.params;
+    const movie = movies.find(function (movie) {
+        return movie.id === Number(id);
+    });
+
+    if (!movie) {
+        return res.status(404).json({
+            message: `La película con el ID ${id} no existe`,
+        })
+    }
+
+    return res.status(200).json(movie);
+});
+
+// -- Rutas de prueba --------------------------------------------------
 app.get('/', function(req, res) {
     return res.status(200).json({
         message: 'Bienvenido a mi API, excepto AMLO y Java'
