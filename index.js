fa56e94b19ea602532b0710c41cd2f89ea5ea00d
Martin Melo <martin.melo.dev.97@gmail.com>
2024-02-06 18:32:40 -0600

    Fixed nomemon dep type, installed dotenv and .env file created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
+
+app.get('/', function(req, res) {
+    return res.status(200).json({
+        message: 'Bienvenido a mi API, excepto AMLO y Java'
+    })
+});
 
 app.get('/:name', function(req, res) {
     return res.status(200).json({ message: 'Hola Mundo! ' + req.params.name })
 });
 
-app.listen(5000, function() {
-    console.log('La API esta corriendo en el puerto 5000')
+const port = process.env.PORT;
+
+app.listen(port, function() {
+    console.log(`La API esta corriendo en el puerto ${port}`)
 });
