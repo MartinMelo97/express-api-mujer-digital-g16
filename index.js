9b4df7651c764a1b77f34c5ced96a9a1f2dce873
Martin Melo <martin.melo.dev.97@gmail.com>
2024-02-12 19:00:52 -0600

    POST endpoint

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // recibir datos en formato JSON
+app.use(express.urlencoded({ extended: false })) // permitir datos en querystrings
+
 // -- Rutas de películas ------------------------------------------------
 app.get('/movies', function(req, res) {
     return res.status(200).json(movies);
@@ -25,6 +28,25 @@ app.get('/movies/:id', function(req, res) {
 
     return res.status(200).json(movie);
 });
+
+app.post('/movies', function(req, res) {
+    const data = req.body;
+    console.log('informacion que llega', data);
+
+    // TODO: Agregar validación
+    // 1. Que el ID que se envía no esté en otra película
+    // 2. Que el ID sea un número
+    // 3. Que el atribuito name exista, sea un string y tenga al menos 3 caracteres
+    // 4. Que el atributo duracion exista, sea un número y que no sea negativo
+    // 5. La sinopsis puede estar o no estar (opcional)
+    // 6. QUe el atributo cast si existe, sea un arreglo de string
+    // y si no existe, pasar un arreglo vacio
+
+    movies.push(data);
+    return res.status(201).json(movies)
+});
+
+// GET - POST - PUT - PATCH - DELETE
 
 // -- Rutas de prueba --------------------------------------------------
 app.get('/', function(req, res) {
